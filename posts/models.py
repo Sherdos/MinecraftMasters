952a2fe1c34517e60dcfe290728521407bf9685f
sherdos <sherdos26@gmail.com>
2023-09-19 00:01:01 +0600

    added category and pastpost

--- posts/models.py
+++ posts/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     description = models.TextField(verbose_name='описание')
     image = models.ImageField(upload_to='image_post/', verbose_name='постер')
     created = models.DateTimeField(auto_now_add=True, verbose_name='время создание')
+    category = models.ForeignKey('posts.Category', models.CASCADE, verbose_name='категория', null=True)
     
     class Meta:
         verbose_name = 'Пост'
@@ -20,3 +21,31 @@ class Post(models.Model):
     
     def __str__(self) -> str:
         return f'{self.title}'    
+    
+
+class Category(models.Model):
+    
+    title = models.CharField(max_length=50, verbose_name='заголовок')
+    description = models.TextField(verbose_name='описание')
+    url = models.SlugField(max_length=50, verbose_name='путь')
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def get_absolute_url(self):
+        return reverse("category", kwargs={"slug": self.url})
+    
+    def __str__(self) -> str:
+        return f'{self.title}'    
+    
+    
+class PostPart(models.Model):
+    
+    title = models.CharField(max_length=255, verbose_name='под заголовок')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='image_post/', verbose_name='Фото')
+    
+    
+    def __str__(self) -> str:
+        return f'{self.title}'   
